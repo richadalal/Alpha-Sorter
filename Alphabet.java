91eefeb87df1cbf376208ed4641ec322e007a6a3
richadalal <65190493+richadalal@users.noreply.github.com>
2020-12-27 15:43:40 -0500

    Update and rename Names.java to Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,33 @@
+import hsa.Console;
+import java.util.*;
+public class Alphabet
+{ static Console c;
+  public static void main(String[] args)
+  { c = new Console();
+    //creating one array
+    String[] alphabet = new String[10];
+    String answer;  
+    String search;
+    //display the array
+    c.println("Original alphabet array");
+    for(int x = 0; x < 10; x++)
+    {
+      names [x] = c.readLine();
+    }
+    c.println("Here is the alphabet after sorting in ascending order:");
+    Arrays.sort(alphabet); //sort the array in ascending (alphabetical) order
+    //display array
+    for(int x = 0; x < 10; x++)
+    {
+      c.println(alphabet[x]);
+    }
+    c.println("Here is the alphabet after sorting in descending order:");
+    Arrays.sort(alphabet, Collections.reverseOrder()); //sort in descending (reverse alphabetical) order
+    
+     //displays the array after reversing the order 
+    for(int x = 0; x < 10; x++)
+    {
+      c.println(alphabet[x]);
+    }
+    }
+  }
